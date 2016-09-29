5d756019200839b6e9f23c98c6cea0ef79b4e78e
supernova1992 <jimtaylor011@gmail.com>
2016-09-28 19:09:52 -0500

    Added functionality
    Still working towards a fully functional program for RNAi, next is
    Gateway. Other methods may be added later. Gateway and TOPO serving as
    placeholders for now.

--- Main.py
+++ Main.py
@@ -10,27 +10,32 @@ class Application(Frame):
 		self.QUIT["fg"] = "red"
 		self.QUIT["command"] = self.quit
 		
-		self.QUIT.pack({"side": "left"})
+		self.QUIT.grid(row=3,column=1)
 		
 		self.submit_button = Button(self)
 		self.submit_button["text"] = "submit"
-		self.submit_button["command"] = self.submit
+		self.submit_button["command"] = self.submit_method
 		
-		self.submit_button.pack({"side": "left"})
+		self.submit_button.grid(row=3, column=0)
 		
-		self.lab_seq = Label(self,text="Sequence:")
-		self.lab_seq.pack({"side":"left"})
+		self.clone_type = Listbox(self)
+		self.clone_type.grid(row=0,column=0)
 		
-		self.sequence = Entry(self)
-		self.sequence.pack({"side": "left"})
+		for item in ["Gateway","RNAi","TOPO"]:
+			self.clone_type.insert(END, item)
 	
 	def __init__(self, master=None):
 		Frame.__init__(self, master)
-		self.pack()
+		self.grid()
 		self.createWidgets()
+	def forget_me(self, stuff):
+		for item in stuff:
+			item.grid_forget()
+	
 	
 	def clean_sequence(self, sequence):
 			"Cleans non-sequence characters from the sequence"
+			#old method of cleaning input characters
 			#delete_table = maketrans(
 			#			"ACGTacgt", " " * len("ACGTacgt")
 			#)
@@ -67,9 +72,67 @@ class Application(Frame):
 					print("One or more of the enzymes could not be found in the vector")
 
 			return
-	def submit(self):
-		test = self.sequence.get()
+	def RNAi(self):
+		"Creates a vector for RNA interference of a targeted gene"
+		
+		
+		
+		
+		
+		
+	def clone_logic(self, type):
+			"Changes the cloning method"
+			if type == "RNAi":
+				print("You have selected RNAi")
+				#self.lab_seq.grid_forget()
+				#self.vector_sequence.grid_forget()
+				#self.button_method.grid_forget()
+				forget = [self.lab_seq,self.vector_sequence,self.button_method]
+				self.forget_me(forget)
+				
+				
+				self.button_method = Button(self)
+				self.button_method["text"] = "submit"
+				self.button_method["command"] = self.RNAi
+				self.button_method.grid(row=3, column=0)
+				
+			elif type == "Gateway":
+				print("You have selected Gateway Cloning")
+				self.Gateway()
+			elif type == "TOPO":
+				print("You have selected TOPO Cloning")
+				self.TOPO()
+				
+				
+	def submit_vector(self):
+		test = self.vector_sequence.get("1.0",END)
 		print(self.clean_sequence(test))
+		type = self.clone_type.get(self.clone_type.curselection())
+		self.clone_logic(type)
+		
+		
+	def submit_method(self):
+		method = self.clone_type.get(self.clone_type.curselection())
+		print(method)
+		
+		#self.clone_type.grid_forget()
+		#self.submit_button.grid_forget()
+		forget = [self.clone_type,self.submit_button]
+		self.forget_me(forget)
+		
+		self.lab_seq = Label(self,text="Vector Sequence:")
+		self.lab_seq.grid(row=1, column=0)
+		
+		self.vector_sequence = Text(self)
+		self.vector_sequence.grid(row=2,column=0)
+		
+		self.button_method = Button(self)
+		self.button_method["text"] = "submit"
+		self.button_method["command"] = self.submit_vector
+		self.button_method.grid(row=3, column=0)
+		
+		
+		
 root = Tk()
 app = Application(master=root)
 app.mainloop()
